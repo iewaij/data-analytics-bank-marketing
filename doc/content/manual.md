028561da42e7e288a83819af1b69dd2101ca39ec
Jiawei Li <iewaijil@gmail.com>
2020-10-04 13:09:40 +0200

    Update doc

--- doc/content/manual.md
+++ doc/content/manual.md
@@ -1,26 +1,3 @@
----
-title: Data Analytics Project
-author: 
-- Fan Jia
-- Jiawei Li
-- Strahinja Trenkic
-- Qiqi Zhou
-geometry:
-- top=20mm
-- left=20mm
-- right=20mm
-- bottom=20mm
-- heightrounded
-toc: yes
-toc-depth: 2
-numbersections: yes
-secnumdepth: 2
-mainfont: Latin Modern Roman
-monofont: JetBrains Mono
-papersize: a4
-dpi: 300
----
-\newpage
 # Data Preparation
 
 ```python
--- utility/convert.sh
+++ utility/convert.sh
@@ -2,25 +2,25 @@
 
 jupytext --to md --update-metadata '{"jupytext": {"notebook_metadata_filter":"-all", "cell_metadata_filter":"-all"}}' \
 ../code/1_data_preparation.ipynb \
--o ../doc/source/1_data_preparation.md
+-o ../doc/content/1_data_preparation.md
 
 jupytext --to md --update-metadata '{"jupytext": {"notebook_metadata_filter":"-all", "cell_metadata_filter":"-all"}}' \
 ../code/2_exploratory_data_analysis.ipynb \
--o ../doc/source/2_exploratory_data_analysis.md
+-o ../doc/content/2_exploratory_data_analysis.md
 
 jupytext --to md --update-metadata '{"jupytext": {"notebook_metadata_filter":"-all", "cell_metadata_filter":"-all"}}' \
 ../code/3_tree_based_models.ipynb \
--o ../doc/source/3_tree_based_models.md
+-o ../doc/content/3_tree_based_models.md
 
 jupytext --to md --update-metadata '{"jupytext": {"notebook_metadata_filter":"-all", "cell_metadata_filter":"-all"}}' \
 ../code/4_nerual_network.ipynb \
--o ../doc/source/4_nerual_network.md
+-o ../doc/content/4_nerual_network.md
 
 jupytext --to md --update-metadata '{"jupytext": {"notebook_metadata_filter":"-all", "cell_metadata_filter":"-all"}}' \
 ../code/5_support_vector_machine.ipynb \
--o ../doc/source/5_support_vector_machine.md
+-o ../doc/content/5_support_vector_machine.md
 
-cd ../doc/source
-cat *.md > ../manual.md
+cd ../doc/content
+cat 1_data_preparation.md 2_exploratory_data_analysis.md 3_tree_based_models.md 4_nerual_network.md 5_support_vector_machine.md > manual.md
 cd ../
-pandoc manual.md -o manual.pdf --pdf-engine=xelatex
+pandoc content/manual.md --pdf-engine=xelatex --metadata-file=format/manual.yaml --template=template/manual.latex -o manual.pdf
