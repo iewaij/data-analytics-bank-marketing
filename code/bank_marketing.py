ac60e0200a8217446fab20ad0ba6614c5e965191
Jiawei Li <iewaijil@gmail.com>
2020-11-09 22:52:47 +0100

    Update feature engineering

--- code/bank_marketing.py
+++ code/bank_marketing.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, average_precision_score, f1_score, precision_score, recall_score, balanced_accuracy_score, roc_auc_score, confusion_matrix
 # Plot Libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -22,7 +23,6 @@ rc = {"figure.figsize": (6.4, 4.8),
       "axes.titleweight": "bold",
       "axes.titlepad": 12,
       "axes.titlelocation": "left"}
-
 sns.set_theme(context="notebook", style="darkgrid", color_codes=True, rc=rc)
 
 def import_dataset(filename):
@@ -120,8 +120,8 @@ def split_dataset(data, preprocessor, random_state=82):
     """
     train_test_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=random_state)
     for train_index, test_index in train_test_split.split(data.drop("y", axis=1), data["y"]):
-        train_set = data.loc[train_index].sort_index()
-        test_set = data.loc[test_index].sort_index()
+        train_set = data.iloc[train_index]
+        test_set = data.iloc[test_index]
 
     y_train = train_set["y"].astype("int").to_numpy()
     y_test = test_set["y"].astype("int").to_numpy()
@@ -130,8 +130,8 @@ def split_dataset(data, preprocessor, random_state=82):
         
     train_validate_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
     for ttrain_index, validate_index in train_validate_split.split(X_train, y_train):
-        ttrain_set = train_set.loc[ttrain_index].sort_index()
-        validate_set = train_set.loc[validate_index].sort_index()
+        ttrain_set = train_set.iloc[ttrain_index]
+        validate_set = train_set.iloc[validate_index]
     
     y_ttrain = ttrain_set["y"].astype("int").to_numpy()
     y_validate = validate_set["y"].astype("int").to_numpy()
@@ -179,14 +179,10 @@ def benchmark(data, preprocessor, clf):
         
     for name, X, y in zip(set_names, X_sets, y_sets):
         # Re-fit model on train set before test set evaluation except CatBoost
-        if name == "Test":
-            try:
-                clf.fit(X_ttrain, y_ttrain, eval_set=(X_validate, y_validate), verbose=False)
-            except (ValueError, TypeError):
-                clf.fit(X_train, y_train)
-                
+        if name == "Test" and not isinstance(clf, CatBoostClassifier):
+            clf.fit(X_train, y_train)
         y_pred = clf.predict(X)
-        
+
         try:
             y_score = clf.decision_function(X)
         except AttributeError:
@@ -202,3 +198,88 @@ def benchmark(data, preprocessor, clf):
         metric_df[name] = metrics
         
     return metric_df
+    
+def cat_encode(X,
+               drop=["duration", "y"],
+               cut=None,
+               external=None,
+               cyclic=None,
+               target=None):
+    """
+    Encode and transform categorical data into numerical values.
+    
+    Parameters
+    ----------
+        X : DataFrame
+        
+        drop : list, default = ["duration", "y"]
+        
+        cut : list
+        
+        external : list
+        
+        cyclic : list
+        
+        target : list
+    
+    Returns
+    -------
+        X : DataFrame
+
+    Examples
+    --------
+    bank_mkt = import_dataset("../data/BankMarketing.csv")
+    X = cat_encode(bank_mkt)
+    """
+    X = X.copy()
+
+    if cut != None:
+        if "pdays" in cut:
+            # Cut pdays into categories
+            X["pdays"] = pd.cut(X["pdays"], [0, 3, 5, 10, 15, 30, 1000], labels=[3, 5, 10, 15, 30, 1000], include_lowest=True).astype("Int64")
+    
+    # Clients who have been contacted but do not have pdays record,
+    # should be encoded as 999
+    X["pdays"] = X["pdays"].fillna(-1)
+    X.loc[X["pdays"].isna() & X["poutcome"].notna(), "pdays"] = 999
+    
+    if external != None:
+        if "year" in external:
+            X.loc[X.index < 27682, "year"] = 2008
+            X.loc[(27682<=X.index) & (X.index<39118), "year"] = 2009
+            X.loc[39118<=X.index, "year"] = 2010
+            X["year"] = X["year"].astype("int")
+        if "econ" in external:
+            X.loc[X.index < 27682, "year"] = 2008
+            X.loc[(27682<=X.index) & (X.index<39118), "year"] = 2009
+            X.loc[39118<=X.index, "year"] = 2010
+            X["year"] = X["year"].astype("int")
+            X["date"] = pd.to_datetime(X[["month", "year"]].assign(day=1))
+            econ = pd.read_csv("../data/Econ.csv")
+            econ["date"] = pd.to_datetime(econ["date"])
+            X = X.merge(econ, how="left", on="date")
+
+    if cyclic != None:
+        if "month" in cyclic:
+            X['month_sin'] = np.sin(2 * np.pi * X["month"]/12)
+            X['month_cos'] = np.cos(2 * np.pi * X["month"]/12)
+            X = X.drop("month", axis=1)
+        if "day_of_week" in cyclic:
+            X['day_sin'] = np.sin(2 * np.pi * X["day_of_week"]/5)
+            X['day_cos'] = np.cos(2 * np.pi * X["day_of_week"]/5)
+            X = X.drop("day_of_week", axis=1)
+            
+    # Transform target encoded feature as str
+    if target != None:
+        X[target] = X[target].astype("str")
+        
+    # Drop features
+    X = X.drop(drop, axis=1)
+    
+    # Other categorical features will be coded as its order in pandas categorical index
+    X = X.apply(lambda x: x.cat.codes if pd.api.types.is_categorical_dtype(x) else (x.astype("Int64") if pd.api.types.is_bool_dtype(x) else x))
+    
+    # Fill missing values as -1
+    X = X.fillna(-1)
+    
+    return X
