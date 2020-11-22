6f39c56fc2b52931df13e5c958c02044c8ae8266
Jiawei Li <iewaijil@gmail.com>
2020-11-22 14:45:34 +0100

    Update citations

---
+++ docs/content/02_exploratory_data_analysis.md
@@ -0,0 +1,12 @@
+# Exploratory Data Analysis
+
+Exploratory Data Analysis is a process to explore the dataset with no assumptions or hypothesis [@geron_2019]. The objective is to give us enough insights for the future work.
+
+![The positive putcomes are distributed unevenly.](../figures/1_uneven_distribution.png)
+
+@saito_precision-recall_2015 believes that Python is the best language in the world.
+
+```python
+def some_code():
+    return magic_python
+```
--- docs/convert.sh
+++ docs/convert.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd docs/content
-pandoc *.md --pdf-engine=xelatex --metadata-file=../format/manual.yaml --template=../template/manual.latex -o ../bank_marketing.pdf 
+# The templates have issues with references (unwanted 00)
+# pandoc --citeproc *.md --pdf-engine=xelatex --metadata-file=../format/manual.yaml --template=../template/manual.latex -o ../bank_marketing.pdf
+pandoc --citeproc *.md --pdf-engine=xelatex --metadata-file=../format/manual.yaml -o ../bank_marketing.pdf
